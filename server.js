f1333082f2b12637910a362a46f628e40e7b0e64
Steven Zhang <stevenzh2012@gmail.com>
2015-10-05 00:36:07 -0500

    exclude swps

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http)
 
 app.get('/', function(req, res){
-    res.send(__dirname + '/index.html');
+    res.sendFile(__dirname + '/index.html');
 });
 
 io.on('connection', function(socket){
